a96e0b61bea1dcefe3725499879e02bdbed5f448
Adam <adam@node-1w7jr9plniwjfgw1lx00kikf0.ipv6.telus.net>
2023-02-24 16:29:28 -0800

    cleanup

--- Snes/SysClockObserver/SysClockObserver.cpp
+++
@@ -1,9 +0,0 @@
-#include "SysClockObserver.hpp"
-
-namespace snes {
-SysClockObserver::SysClockObserver(std::mutex &mutex, std::condition_variable *cv)
-    : lock(mutex), condition(cv) {}
-
-void SysClockObserver::wait(void) { condition->wait(lock); }
-
-}  // namespace snes
